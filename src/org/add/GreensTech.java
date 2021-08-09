9641e7a64f036d066f9ed795198d26418956517a
vignesh2730 <vicky777akon@gmail.com>
2021-08-09 18:32:22 +0530

    sai project

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,28 @@
+package org.add;
+
+public class GreensTech {
+	
+	public void greensOmr() {
+		System.out.println("Greens OMR");
+	}
+
+	public void grennsAdaya() {
+		System.out.println("greens adayar");
+		}
+	
+	public void greensTambaram() {
+		System.out.println("greens tambaram");
+	}
+	
+	public void greensAnnanagar() {
+		System.out.println("greens Annanaga");
+				
+	}
+	public static void main(String[]args) {
+		GreensTech o=new GreensTech();
+		o.greensAnnanagar();
+		o.grennsAdaya();
+		o.greensOmr();
+		o.greensTambaram();
+	}
+}
